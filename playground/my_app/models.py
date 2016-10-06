806b306e099ffae430aedf659ac29bcddfc1faaf
Rick-and-morty <te5840@gmail.com>
2016-10-05 21:02:29 -0400

    week 4 django

---
+++ playground/my_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Fear(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Person(models.Model):
+
+    name = models.CharField(max_length=30)
+    age = models.IntegerField()
+    eye_color = models.CharField(max_length=20)
+    best_friend = models.ForeignKey("self", null=True)
+    fears = models.ManyToManyField(Fear)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.IntegerField()
+    captain = models.ForeignKey(Person)
